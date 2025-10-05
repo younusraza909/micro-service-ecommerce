a916fe9f421a5f94d4cea8d28039f6f069b29940
younusraza909 <younus@geeksofkolachi.com>
2025-10-05 16:09:05 +0500

    Enhance admin panel with new sidebar components and collapsible functionality. Introduce Radix UI components for improved UI elements, including collapsible sections, tooltips, and separators. Update layout to manage sidebar state with cookies for better user experience.

--- admin/src/components/Navbar.tsx
+++ admin/src/components/Navbar.tsx
@@ -1,29 +1,17 @@
 "use client";
 
-import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
 import Link from "next/link";
 import React from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "./ui/button";
-import { useTheme } from "next-themes";
+
 import ThemeToggleMenu from "./ThemeToggleMenu";
 import UserMenu from "./UserMenu";
+import { SidebarTrigger } from "./ui/sidebar";
 
 const Navbar = () => {
-  const { theme, setTheme } = useTheme();
-
   return (
     <nav className="p-4 flex items-center justify-between">
       {/* LEFT */}
-      Collapsed Button
+      <SidebarTrigger />
       {/* RIGHT */}
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
